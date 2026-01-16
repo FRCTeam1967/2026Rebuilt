1a0a534f712df90c1b5bd865d95b2b365aee437c
TaraTony <tonytara567@gmail.com>
2026-01-15 18:38:34 -0800

    Update Constants.java

--- 2026Rebuilt/src/main/java/frc/robot/Constants.java
+++ 2026Rebuilt/src/main/java/frc/robot/Constants.java
@@ -9,21 +9,6 @@ public class Constants {
     public static final int kDriverControllerPort = 0;
     }
 
-public static class Pivot {
-    public static final int PIVOT_MOTOR_ID = 100000;
-
-    public static final int kS = 0;
-    public static final int kV = 0;
-    public static final int kA = 0;
-    public static final int kP = 0;
-    public static final int kI = 0;
-    public static final int kD = 0;
-    
-    public static final int CRUISE_VELOCITY = 0;
-    public static final int ACCELERATION = 0;
-    public static final int JERK = 0;
-    public static final int GEAR_RATIO = 1;
-}
  public static class Climb {
     public static final int RIGHT_MOTOR_ID = 90;
     public static final int LEFT_MOTOR_ID = 99;
