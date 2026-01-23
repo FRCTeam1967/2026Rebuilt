945ac7da0ce8ee0177b312ebecee3425915db0de
shriyankah <shiremagalur27@ndsj.org>
2026-01-22 19:13:50 -0800

    (FINAL) ready-to-test code for wooden jankybot
    Co-Authored-By: nandini-raghavan <185436585+nandini-raghavan@users.noreply.github.com>
    Co-Authored-By: sanaya-vepa <185436582+sanaya-vepa@users.noreply.github.com>
    Co-Authored-By: Nandita-Rallapalli <233176140+Nandita-Rallapalli@users.noreply.github.com>
    Co-Authored-By: Nahily Yadeta :D <149740319+Nahily-Y@users.noreply.github.com>

--- 2026Rebuilt/src/main/java/frc/robot/commands/RunFlywheelShooter.java
+++ 2026Rebuilt/src/main/java/frc/robot/commands/RunFlywheelShooter.java
@@ -3,31 +3,45 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.FlywheelShooter;
+import java.util.function.DoubleSupplier;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class RunFlywheelShooter extends Command {
   /** Creates a new RunFlywheelShooter. */
-  public RunFlywheelShooter() {
-    // Use addRequirements() here to declare subsystem dependencies.
+
+  private final FlywheelShooter shooter;
+  private final double speed;
+    
+  public RunFlywheelShooter(FlywheelShooter shooter, double speed) {
+    this.shooter = shooter;
+    this.speed = speed;
+
+    addRequirements(shooter);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  
+  }
 
-  // Called every time the scheduler runs while the command is scheduled.
+   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    shooter.setMotor(speed);
+  }
+  
+   @Override
+  public void end(boolean interrupted) {
+    shooter.stopMotor();
+  }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
+ // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    //return intake.intakeBroken(); 
     return false;
   }
 }
--- 2026Rebuilt/src/main/java/frc/robot/subsystems/FlywheelShooter.java
+++ 2026Rebuilt/src/main/java/frc/robot/subsystems/FlywheelShooter.java
@@ -4,11 +4,82 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVelocityDutyCycle;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class FlywheelShooter extends SubsystemBase {
+
+  private TalonFX flywheelMotor;
+
   /** Creates a new FlywheelShooter. */
-  public FlywheelShooter() {}
+  public FlywheelShooter() {
+    flywheelMotor = new TalonFX(Constants.FlywheelShooter.FLYWHEELSHOOTER_MOTOR_ID);
+
+      var talonFXConfigs = new TalonFXConfiguration();
+
+      var slot0Configs = talonFXConfigs.Slot0;
+      slot0Configs.kS = Constants.FlywheelShooter.kS; 
+      slot0Configs.kV = Constants.FlywheelShooter.kV; 
+      slot0Configs.kA = Constants.FlywheelShooter.kA; 
+      slot0Configs.kP = Constants.FlywheelShooter.kP; 
+      slot0Configs.kI = Constants.FlywheelShooter.kI;
+      slot0Configs.kD = Constants.FlywheelShooter.kD; 
+  
+      // set Motion Magic settings
+      var motionMagicConfigs = talonFXConfigs.MotionMagic;
+      motionMagicConfigs.MotionMagicCruiseVelocity = Constants.FlywheelShooter.CRUISE_VELOCITY;
+      motionMagicConfigs.MotionMagicAcceleration = Constants.FlywheelShooter.ACCELERATION;
+      motionMagicConfigs.MotionMagicJerk = Constants.FlywheelShooter.JERK;
+
+      talonFXConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+
+      flywheelMotor.setNeutralMode(NeutralModeValue.Brake);
+      //talonFXConfigs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+      flywheelMotor.getConfigurator().apply(talonFXConfigs);
+  }
+
+  public void setMotor(double speed) {
+      flywheelMotor.set(speed);
+  }
+
+  //public void moveTo(double revolutions) {
+      //MotionMagicVoltage request = (new MotionMagicVoltage(revolutions)).withFeedForward(0.0);
+      //flywheelMotor.setControl(request);
+  //}
+
+  public void setVelocity(double velocity) {
+      //MotionMagicVelocityDutyCycle request = new MotionMagicVelocityDutyCycle(velocity);
+      MotionMagicVelocityVoltage request = new MotionMagicVelocityVoltage(velocity);
+
+      flywheelMotor.setControl(request);
+  }
+
+  //public void moveMotor(double speed) {
+      //VelocityVoltage request = new VelocityVoltage(speed);
+      //intakeMotor.setControl(request);
+   //}
+  
+   /** Stops both the left motor and right motor */
+   public void stopMotor() {
+      flywheelMotor.stopMotor();
+   }
+
+
 
   @Override
   public void periodic() {
