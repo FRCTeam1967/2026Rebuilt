e681ca51b7e86347d9d26af235646a9cc6901ef8
sana khan <sanakhantiwary@gmail.com>
2026-02-07 14:41:07 -0800

    test code for conditional command
    Co-Authored-By: eva yin <185436685+evaa-yin@users.noreply.github.com>
    Co-Authored-By: arushi-shah <149740330+arushi-shah@users.noreply.github.com>
    Co-Authored-By: tara-balaji <233176234+tara-balaji@users.noreply.github.com>
    Co-Authored-By: sahasrapothula <185436633+sahasrapothula@users.noreply.github.com>
    Co-Authored-By: Michelle Wu <233176081+mwu29-1967@users.noreply.github.com>

--- 2026Rebuilt/src/main/java/frc/robot/RobotContainer.java
+++ 2026Rebuilt/src/main/java/frc/robot/RobotContainer.java
@@ -11,14 +11,19 @@ import choreo.auto.AutoChooser;
 import choreo.auto.AutoFactory;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 
 import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Percent;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -26,6 +31,7 @@ import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.LED;
 
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
@@ -43,6 +49,11 @@ public class RobotContainer {
     private final CommandXboxController joystick = new CommandXboxController(0);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+    public LED ledSubsystem = Robot.ledSubsystem;
+    LEDPattern solidBlue = LEDPattern.solid(Color.kWhite);
+    LEDPattern blinking = solidBlue.blink(Seconds.of(0.5)).atBrightness(Percent.of(10));
+    Command blinkCommand = ledSubsystem.runPattern(blinking).ignoringDisable(true);
+
     //public static SendableChooser<Command> autoChooser; 
     //public ShuffleboardTab matchTab = Shuffleboard.getTab("Match");
     
@@ -78,6 +89,8 @@ public class RobotContainer {
         joystick.b().whileTrue(drivetrain.applyRequest(() ->
             point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
         ));
+
+        joystick.x().whileTrue(ledSubsystem.runPattern(blinking));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
