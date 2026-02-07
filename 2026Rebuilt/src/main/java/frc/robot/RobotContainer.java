8cba1cb53399a4a5c04a28a77745172dc8801b8d
Nahily-Y <nyadeta27@ndsj.org>
2026-02-07 13:34:42 -0800

    LED code with different added patterns
    We tested this code on Lupe and added some different patterns like Larson, colorflow, and twinkle. They are all tested and should work, and we also plan on adding more, like solid color LEDs.
    
    Co-Authored-By: Nandita-Rallapalli <233176140+Nandita-Rallapalli@users.noreply.github.com>
    Co-Authored-By: nandini-raghavan <185436585+nandini-raghavan@users.noreply.github.com>
    Co-Authored-By: Shriyanka Hiremagalur <149740320+shriyankah@users.noreply.github.com>
    Co-Authored-By: sanaya-vepa <185436582+sanaya-vepa@users.noreply.github.com>

--- 2026Rebuilt/src/main/java/frc/robot/RobotContainer.java
+++ 2026Rebuilt/src/main/java/frc/robot/RobotContainer.java
@@ -3,10 +3,10 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
-import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.LED;
 
 public class RobotContainer {
-  private final LEDSubsystem leds = new LEDSubsystem();
+  private final LED leds = new LED();
   private final CommandXboxController driver = new CommandXboxController(0);
 
   public RobotContainer() {
@@ -16,5 +16,8 @@ public class RobotContainer {
   private void configureBindings() {
     driver.a().onTrue(new InstantCommand(leds::setRainbow, leds));
     driver.b().onTrue(new InstantCommand(leds::setFire, leds));
+    driver.x().onTrue(new InstantCommand(leds::setTwinkle, leds));
+    driver.leftBumper().onTrue(new InstantCommand(leds::setColorFlow, leds));
+    driver.rightBumper().onTrue(new InstantCommand(leds::setLarson, leds));
   }
 }
