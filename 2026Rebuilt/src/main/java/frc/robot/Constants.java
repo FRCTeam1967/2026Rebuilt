a743e771ff30bdc96675784aa26156e261a0385e
kaytki-chak <149740324+kaytki-chak@users.noreply.github.com>
2026-02-07 14:12:51 -0800

    deleted pooper and updated gear ratio and motor ids
    Co-Authored-By: TaraTony <149740360+TaraTony@users.noreply.github.com>
    Co-Authored-By: sreenikaj <185436675+sreenikaj@users.noreply.github.com>
    Co-Authored-By: rayaproggy <185436778+rayaproggy@users.noreply.github.com>
    Co-Authored-By: Azalia Martinez <233176303+azaliamartinez@users.noreply.github.com>
    Co-Authored-By: arsheta <185436702+arsheta@users.noreply.github.com>

--- 2026Rebuilt/src/main/java/frc/robot/Constants.java
+++ 2026Rebuilt/src/main/java/frc/robot/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
         public static final int kOperatorControllerPort = 1;
     }
     public static class Extension{
-        public static final int EXTENSION_MOTOR_ID = 100000;
+        public static final int EXTENSION_MOTOR_ID = 10;
 
         public static final double kS = 0.25;
         public static final double kV = 0.12;
@@ -19,25 +19,19 @@ public class Constants {
         public static final int CRUISE_VELOCITY = 40;
         public static final int ACCELERATION = 100;
         public static final int JERK = 1000;
-        public static final double GEAR_RATIO = 21/1.0;
+        public static final double GEAR_RATIO = 16/1.0;
         public static final double FEED_FORWARD = 1;
 
     }
 
     public static class Intake{
-        public static final int INTAKE_MOTOR_ID = 9;
+        public static final int INTAKE_MOTOR_ID = 11;
         public static final int INTAKE_MOTOR_SPEED = 10;
     }
 
     public static class Indexer{
-        public static final int INDEXER_MOTOR_ID = 9;
+        public static final int INDEXER_MOTOR_ID = 12;
 
     }
-
-    public static class Pooper{
-        public static final int POOPER_TOP_MOTOR_ID = 9;
-        public static final int POOPER_BOTTOM_MOTOR_ID = 10;
-    }
-
 
 }
